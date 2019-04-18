64399ca1b8620855bde24ad737a907e60c0dba8e
Nolan Kovacik <noltron000@gmail.com>
2019-04-18 15:24:30 -0700

    crafting basic structure

--- App.js
+++ App.js
@@ -1,21 +1,44 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { 
+	StyleSheet,
+	ScrollView,
+	KeyboardAvoidingView,
+	View,
+	Text,
+	TextInput 
+} from 'react-native';
 
 export default class App extends React.Component {
 	render() {
 		return (
-			<View style={styles.container}>
-				<Text>Open up App.js to start working on your app!</Text>
-			</View>
+			<ScrollView style={styles.scrollView}>
+				<KeyboardAvoidingView
+					enabled
+					behavior="position"
+				>
+					<View>
+						<Text>
+							User Name
+						</Text>
+						<TextInput
+							placeholder='user name'
+						/>
+					</View>
+				</KeyboardAvoidingView>
+			</ScrollView>
 		);
 	}
 }
 
 const styles = StyleSheet.create({
-	container: {
-		flex: 1,
+	scrollView: {
 		backgroundColor: '#fff',
-		alignItems: 'center',
-		justifyContent: 'center',
+		// flex: 1,
+		// alignItems: 'center',
+		// justifyContent: 'center',
 	},
+
+	placeholder: {
+		backgroundColor: '#f0f',
+	}
 });
